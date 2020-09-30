f835c16ce8dd8053788c87fe0135a0bd5a4ea9c4
Michael Callahan <CallahanMichaelE@gmail.com>
2020-09-30 12:42:02 -0600

    jest breakpoints now working - moving on to step connecting with mongoDB

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const todoRoutes = require("./routes/todo.routes")
 const app = express();
+
+app.use(express.json());
+
+app.use("/todos", todoRoutes)
 
 app.get('/', (req, res) => {
     res.json("Hello World")
 });
 
-app.listen(3069, () => {
-    console.log("server is now running")
-})
+module.exports = app
